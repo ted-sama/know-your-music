5f67c112fe30e640727b929dc4f6d87b290947c5
Teddy Nsoki <teddynsoki@gmail.com>
2024-09-19 22:32:45 +0200

    project init

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen bg-gradient-to-b from-emerald-900 to-neutral-950 to-30% text-white">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/game/spotidle.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex justify-center items-center pt-32">
+      <h1 class="text-4xl">Spotidle</h1>
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  
+  </script>
+  
+  <style>
+  
+  </style>
